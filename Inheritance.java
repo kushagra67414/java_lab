a08561343ee928bdbf621022e6c2704f3485f8f5
kushagra67414 <500067414@stu.upes.ac.in>
2020-02-13 10:47:55 +0530

    first commit

---
+++ Inheritance.java
@@ -0,0 +1,63 @@
+class room_area
+
+{
+
+    private int l,b;
+
+    private room_area(int x,int y)
+
+    { l=x; b=y;}
+
+    int area()
+
+    {return(l*b);}
+
+}
+
+class class_room extends room_area
+
+{
+
+    int h;
+
+    class_room(int x,int y,int z)
+
+    {
+
+        super(x,y);
+
+        h=z;
+
+    }
+
+    int volume()
+
+    {
+
+        return(area()*h);
+
+    }
+
+}
+
+class Inheritance
+
+{
+
+    public static void main(String args[])
+
+    {
+
+        class_room cr=new class_room(10,20,15);
+
+        int a1=cr.area();
+
+        int v1=cr.volume();
+
+        System.out.println("Area of Room : "+a1);
+
+        System.out.println("Volume of Room : "+v1);
+
+    }
+
+}
---
+++ SumCombinations.java
@@ -0,0 +1,66 @@
+ 
+import java.util.ArrayList;
+import java.util.Arrays;
+ 
+public class SumCombinations {
+ 
+	static void sum_combinations_recursive(ArrayList<Integer> inputNumbers, int targetSum, ArrayList<Integer> partialNumbers) {
+ 
+	       int sum = 0;
+	     	       
+	       //calculate summation of partial numbers
+	       for (int x: partialNumbers) {	 
+   	   
+	    	   sum += x;
+ 
+	       }    
+	      
+	       //check if summation of partial numbers is equal to sum target
+	       if (sum == targetSum)
+	            System.out.println("sum("+Arrays.toString(partialNumbers.toArray())+")="+targetSum);
+	       
+	       //if we sum is greater than the target sum then why to continue 
+	       if (sum >= targetSum){
+ 
+	    	   //ends the current method invocation
+	    	   return;
+	       }	            
+	       
+	       //add the remaining numbers in the partial number list 
+	       for(int i=0;i<inputNumbers.size();i++) {	  
+  	   
+	             ArrayList<Integer> remainingNumbers = new ArrayList<Integer>();
+ 
+	             int n = inputNumbers.get(i);	
+            
+	             for (int j=i+1; j<inputNumbers.size();j++) {
+ 
+	            	 remainingNumbers.add(inputNumbers.get(j));
+	             }
+	             
+	             ArrayList<Integer> partialNumbersList = new ArrayList<Integer>(partialNumbers);
+ 
+	             partialNumbersList.add(n);
+ 
+	             //recursive call
+	             sum_combinations_recursive(remainingNumbers,targetSum,partialNumbersList);
+	       }
+ 
+	    }
+	    static void sum_combinations(ArrayList<Integer> inputNumbers, int targetSum) {
+ 
+	    	sum_combinations_recursive(inputNumbers,targetSum,new ArrayList<Integer>());
+ 
+	    }
+ 
+	    public static void main(String args[]) {
+ 
+	        Integer[] inputNumbers = {1,2,3,4,10,15,20,35,50,100};
+ 
+	        int targetSum = 50;
+ 
+	        sum_combinations(new ArrayList<Integer>(Arrays.asList(inputNumbers)),targetSum);
+ 
+	    }
+ 
+}
